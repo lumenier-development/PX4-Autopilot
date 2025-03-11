a7492d9e357fd3000a44a400079a99466e2658c6
D.J. Morvay <djmorvay@me.com>
2025-03-11 10:53:41 -0400

    Adding intitial build files.

---
+++ boards/lumenier/H743-NDAA/src/board_config.h
@@ -0,0 +1,182 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2019 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/**
+ * @file board_config.h
+ *
+ * holybro KakuteH7 internal definitions
+ */
+
+#pragma once
+
+/****************************************************************************************************
+ * Included Files
+ ****************************************************************************************************/
+
+#include <px4_platform_common/px4_config.h>
+#include <nuttx/compiler.h>
+#include <stdint.h>
+
+#include <stm32_gpio.h>
+
+/****************************************************************************************************
+ * Definitions
+ ****************************************************************************************************/
+
+/* Lumenier H743 - NDAA GPIOs ************************************************************************/
+
+/* LEDs are driven with push open drain to support Anode to 5V or 3.3V */
+
+#define GPIO_nLED_BLUE  /* PE3 */  (GPIO_OUTPUT|GPIO_OPENDRAIN|GPIO_SPEED_50MHz|GPIO_PULLUP|GPIO_OUTPUT_SET|GPIO_PORTE|GPIO_PIN3)
+
+#define BOARD_HAS_CONTROL_STATUS_LEDS	1
+#define BOARD_ARMED_STATE_LED		LED_BLUE
+
+/*
+ * ADC channels
+ *
+ * These are the channel numbers of the ADCs of the microcontroller that
+ * can be used by the Px4 Firmware in the adc driver
+ */
+
+/* ADC defines to be used in sensors.cpp to read from a particular channel */
+
+#define ADC1_CH(n)                  (n)
+
+/* Define GPIO pins used as ADC N.B. Channel numbers must match below */
+#define PX4_ADC_GPIO  \
+	/* PC0 */  GPIO_ADC123_INP10_0, \
+	/* PC1 */  GPIO_ADC123_INP11_0, \
+	/* PC4 */  GPIO_ADC12_INP4_0, \
+	/* PC5 */  GPIO_ADC12_INP8_0
+
+/* Define Channel numbers must match above GPIO pin IN(n)*/
+#define ADC_BATTERY_VOLTAGE_CHANNEL        /* PC0 */  ADC1_CH(10)
+#define ADC_BATTERY_CURRENT_CHANNEL        /* PC1 */  ADC1_CH(11)
+#define ADC_AIRSPEED_IN_CHANNEL            /* PC4 */  ADC1_CH(4)
+#define ADC_RSSI_IN_CHANNEL                /* PC5 */  ADC1_CH(8)
+
+#define ADC_CHANNELS \
+	((1 << ADC_BATTERY_VOLTAGE_CHANNEL)       | \
+	 (1 << ADC_BATTERY_CURRENT_CHANNEL)       | \
+	 (1 << ADC_AIRSPEED_IN_CHANNEL)           | \
+	 (1 << ADC_RSSI_IN_CHANNEL))
+
+#define SYSTEM_ADC_BASE STM32_ADC1_BASE
+
+/* By Providing BOARD_ADC_USB_CONNECTED (using the px4_arch abstraction)
+ * this board support the ADC system_power interface, and therefore
+ * provides the true logic GPIO BOARD_ADC_xxxx macros.
+*/
+#define BOARD_ADC_USB_CONNECTED (px4_arch_gpioread(GPIO_OTGFS_VBUS))
+
+#define UAVCAN_NUM_IFACES_RUNTIME 1
+#define UAVCAN_WAIT_MSR
+
+/* PWM */
+#define DIRECT_PWM_OUTPUT_CHANNELS  12
+#define BOARD_HAS_PWM  DIRECT_PWM_OUTPUT_CHANNELS
+
+/* Spare GPIO */
+#define GPIO_VIDEO_PWR     /* PB2  */  (GPIO_OUTPUT|GPIO_PUSHPULL|GPIO_SPEED_2MHz|GPIO_OUTPUT_CLEAR|GPIO_PORTB|GPIO_PIN2)
+#define GPIO_VIDEO_CAM     /* PD10 */  (GPIO_OUTPUT|GPIO_PUSHPULL|GPIO_SPEED_2MHz|GPIO_OUTPUT_CLEAR|GPIO_PORTD|GPIO_PIN10)
+
+/* Tone alarm output */
+#define GPIO_TONE_ALARM_IDLE    /* PC8 */ (GPIO_OUTPUT|GPIO_PUSHPULL|GPIO_SPEED_2MHz|GPIO_OUTPUT_CLEAR|GPIO_PORTC|GPIO_PIN8)
+#define GPIO_TONE_ALARM_GPIO    /* PC8 */ (GPIO_OUTPUT|GPIO_PUSHPULL|GPIO_SPEED_2MHz|GPIO_OUTPUT_SET|GPIO_PORTC|GPIO_PIN8)
+
+/* High-resolution timer */
+#define HRT_TIMER               3  /* use timer3 for the HRT */
+#define HRT_TIMER_CHANNEL       1  /* use capture/compare channel 1 */
+
+#define BOARD_NUM_IO_TIMERS	3
+
+/* RC Serial port */
+
+/* SD Card */
+#define SDIO_SLOTNO                    0  /* Only one slot */
+#define SDIO_MINOR                     0
+#define MMCSD_DETECT	/* PE12 */     (GPIO_INPUT|GPIO_FLOAT|GPIO_EXTI|GPIO_PORTE|GPIO_PIN12)
+#define GPIO_SDMMC1_NCD		  	MMCSD_DETECT
+
+/* This board provides a DMA pool and APIs */
+#define BOARD_DMA_ALLOC_POOL_SIZE 5120
+
+/* This board provides the board_on_reset interface */
+
+#define BOARD_HAS_ON_RESET 1
+
+#define PX4_GPIO_INIT_LIST { \
+		PX4_ADC_GPIO,                     \
+		GPIO_CAN1_TX,                     \
+		GPIO_CAN1_RX,                     \
+		GPIO_VIDEO_PWR,			  \
+		GPIO_VIDEO_CAM,			  \
+		GPIO_TONE_ALARM_IDLE,             \
+		PX4_MAKE_GPIO_OUTPUT_CLEAR(GPIO_I2C1_SCL), \
+		PX4_MAKE_GPIO_OUTPUT_CLEAR(GPIO_I2C1_SDA), \
+		PX4_MAKE_GPIO_OUTPUT_CLEAR(GPIO_I2C2_SCL), \
+		PX4_MAKE_GPIO_OUTPUT_CLEAR(GPIO_I2C2_SDA), \
+	}
+
+#define BOARD_ENABLE_CONSOLE_BUFFER
+
+__BEGIN_DECLS
+
+/****************************************************************************************************
+ * Public Types
+ ****************************************************************************************************/
+
+/****************************************************************************************************
+ * Public data
+ ****************************************************************************************************/
+
+#ifndef __ASSEMBLY__
+
+/****************************************************************************************************
+ * Public Functions
+ ****************************************************************************************************/
+
+extern void stm32_spiinitialize(void);
+
+extern int stm32_spi_bus_initialize(void);
+
+extern void stm32_usbinitialize(void);
+
+extern void board_peripheral_reset(int ms);
+
+#include <px4_platform_common/board_common.h>
+
+#endif /* __ASSEMBLY__ */
+
+__END_DECLS
---
+++ boards/lumenier/H743-NDAA/src/spi.cpp
@@ -0,0 +1,200 @@
+/****************************************************************************
+ *
+ *   Copyright (C) 2020, 2021 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#include <px4_platform_common/px4_config.h>
+#include <px4_platform/gpio.h>
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <debug.h>
+#include <unistd.h>
+#include <cstdio>
+
+#include <nuttx/spi/spi.h>
+#include <nuttx/mtd/mtd.h>
+#include <nuttx/fs/fs.h>
+#include <nuttx/fs/nxffs.h>
+#include <arch/board/board.h>
+#include <systemlib/px4_macros.h>
+#include <nuttx/mmcsd.h>
+
+#include <arm_internal.h>
+#include <chip.h>
+#include <stm32_gpio.h>
+#include "board_config.h"
+
+#include <px4_arch/spi_hw_description.h>
+#include <arch/board/board.h>
+#include <px4_platform_common/log.h>
+#include <drivers/drv_sensor.h>
+#include <nuttx/spi/spi.h>
+
+#ifndef MODULE_NAME
+#define MODULE_NAME "spi_init"
+#endif
+
+constexpr px4_spi_bus_t px4_spi_buses[SPI_BUS_MAX_BUS_ITEMS] = {
+	initSPIBus(SPI::Bus::SPI1, {
+		initSPIDevice(SPIDEV_FLASH(0), SPI::CS{GPIO::PortA, GPIO::Pin4})
+	}),
+	initSPIBus(SPI::Bus::SPI2, {
+		initSPIDevice(SPIDEV_MMCSD(0), SPI::CS{GPIO::PortA, GPIO::Pin10}),
+	}),
+	initSPIBus(SPI::Bus::SPI3, {
+		initSPIDevice(DRV_IMU_DEVTYPE_ICM45686, SPI::CS{GPIO::PortA, GPIO::Pin15}, SPI::DRDY{GPIO::PortD, GPIO::Pin11}),
+	}),
+	initSPIBus(SPI::Bus::SPI4, {
+		initSPIDevice(DRV_OSD_DEVTYPE_ATXXXX, SPI::CS{GPIO::PortE, GPIO::Pin4}),
+	}),
+	initSPIBus(SPI::Bus::SPI6, {
+		initSPIDevice(DRV_IMU_DEVTYPE_ICM42688P, SPI::CS{GPIO::PortD, GPIO::Pin7}, SPI::DRDY{GPIO::PortC, GPIO::Pin13}),
+	}),
+};
+
+static constexpr bool unused = validateSPIConfig(px4_spi_buses);
+
+/************************************************************************************
+ * Name: stm32_spi_bus_initialize
+ *
+ * Description:
+ *   Called to configure SPI buses on LUX F765 - NDAA board.
+ *
+ ************************************************************************************/
+static struct spi_dev_s *spi_expansion;
+
+__EXPORT int stm32_spi_bus_initialize(void)
+{
+	struct spi_dev_s *spi;
+	struct mtd_dev_s *mtd;
+	int ret = OK;
+	#if defined(CONFIG_FS_NXFFS) || defined(CONFIG_FS_LITTLEFS)
+	char devname[12];
+	#endif
+
+	/* Configure SPI-based devices */
+
+	/* Configure MMCSD on SPI2 */
+	/* Get the external SPI port */
+	spi_expansion = stm32_spibus_initialize(SDCARD_SPI_BUS);
+
+	if (!spi_expansion) {
+		syslog(LOG_ERR, "[boot] FAILED to initialize SPI port %d\n", 1);
+		return -ENODEV;
+	}
+
+	#ifdef CONFIG_MMCSD
+		ret = mmcsd_spislotinitialize(CONFIG_NSH_MMCSDMINOR, CONFIG_NSH_MMCSDSLOTNO, spi_expansion);
+
+		if (ret != OK) {
+			syslog(LOG_ERR, "[boot] FAILED to bind SPI port 1 to the MMCSD driver\n");
+			return -ENODEV;
+		}
+
+	#endif
+
+	/* Configure Flash on SPI1 */
+	spi = stm32_spibus_initialize(FLASH_SPI_BUS);
+	if (!spi)
+	{
+		ferr("ERROR: Failed to initialize SPI port 2\n");
+		return -ENODEV;
+	}
+
+	/* Now bind the SPI interface to the W25 SPI FLASH driver */
+
+	mtd = w25_initialize(spi);
+	if (!mtd)
+	{
+		ferr("ERROR: Failed to bind SPI port 2 to the W25 FLASH driver\n");
+		return -ENODEV;
+	}
+
+
+	#ifdef CONFIG_FS_NXFFS
+		/* Initialize to provide NXFFS on the MTD interface */
+
+		ret = nxffs_initialize(mtd);
+		if (ret < 0)
+		{
+			ferr("ERROR: NXFFS initialization failed: %d\n", -ret);
+			return ret;
+		}
+
+		/* Mount the file system at /mnt/w25 */
+
+		snprintf(devname, 12, "/mnt/w25%c", 'a' + 0);
+		ret = nx_mount(NULL, devname, "nxffs", 0, NULL);
+		if (ret < 0)
+		{
+			ferr("ERROR: Failed to mount the NXFFS volume: %d\n", ret);
+			return ret;
+		}
+
+	#elif defined(CONFIG_FS_LITTLEFS)
+		/* Initialize to provide LittleFS on the MTD interface */
+		/* Configure the device with no partition support */
+
+		snprintf(devname, sizeof(devname), "/dev/w25%s","lfs");
+
+		ret = register_mtddriver(devname, mtd, 0755, NULL);
+
+		if (ret != OK) {
+			PX4_ERR("register_mtddriver() failed: %d", ret);
+
+		} else {
+			ret = nx_mount(devname, "/mnt/w25q", "littlefs", 0, NULL);
+
+			if (ret < 0) {
+				ret = nx_mount(devname, "/mnt/w25q", "littlefs", 0,
+						"forceformat");
+
+				if (ret < 0) {
+					PX4_ERR("W25 mount failure: %d", ret);
+
+				} else {
+					PX4_INFO("W25 was forceformatted!");
+				}
+			}
+		}
+	#else
+		/* And use the FTL layer to wrap the MTD driver as a block driver */
+		ret = ftl_initialize(0, mtd);
+		if (ret < 0)
+		{
+			ferr("ERROR: Initialize the FTL layer\n");
+			return ret;
+		}
+	#endif
+
+	return OK;
+}
